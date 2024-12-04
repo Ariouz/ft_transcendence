550b4a2991b647e72ed9e40754332a6c5dd7602c
vicalvez <vicalvez@42nice.fr>
2024-12-04 14:33:41 +0100

    Moved Django Secrets to .env

--- websocket_server/websocket_server/websocket_server/settings.py
+++ websocket_server/websocket_server/websocket_server/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jv-aytvfx9r7nt!6x)0@9uz-z^np*gby199g8t4cah^x7o&gq+'
+SECRET_KEY = os.getenv("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
